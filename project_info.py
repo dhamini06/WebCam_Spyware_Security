3322bda39d6a8e98a650290a5633eeebbdc559ac
dhamini06 <dhamini467@gmail.com>
2026-08-04 10:57:10 +0530

    Format Sadiq and Harsha employee IDs as ST#IS# format

--- project_info.py
+++ project_info.py
@@ -23,8 +23,8 @@ COMPANY_EMAIL = "contact@suprajatechnologies.com"
 DEVELOPERS = [
     ("K. Dhamini", "ST#IS#9540", "dhamini467@gmail.com"),
     ("Ande Manjunath", "ST#IS#9533", "manjunath13556d@gmail.com"),
-    ("Shaik Mohammad Sadiq", "9565", "Sadiqshaik0402@gmail.com"),
-    ("Kadire Harsha Vardhan", "9566", "harshayadavkadire@gmail.com"),
+    ("Shaik Mohammad Sadiq", "ST#IS#9565", "Sadiqshaik0402@gmail.com"),
+    ("Kadire Harsha Vardhan", "ST#IS#9566", "harshayadavkadire@gmail.com"),
 ]
 
 _HTML_TEMPLATE = """<!DOCTYPE html>
